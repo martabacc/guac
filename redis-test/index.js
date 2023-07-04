510fc256f78605c4564f26f32130f76d7e1fbb75
Ronauli Silva <ronauli.silva@gojek.com>
2023-07-04 15:07:53 +0700

    feat(master): wrap async

--- redis-test/index.js
+++ redis-test/index.js
@@ -3,7 +3,8 @@ const Redis = require('ioredis');
 console.log(
     process.env.SENTINEL_1,
     process.env.SENTINEL_2,
-    process.env.SENTINEL_3
+    process.env.SENTINEL_3,
+    MASTER_SENTINEL
 );
 // Redis Sentinel configuration
 const sentinelOptions = {
@@ -15,36 +16,36 @@ const sentinelOptions = {
     name: process.env.MASTER_SENTINEL
 };
 
-console.log(sentinelOptions)
-// Create Redis Sentinel client
-const sentinelClient = new Redis(sentinelOptions);
-
-// Connect to the Sentinel
-sentinelClient.on('connect', () => {
-    console.log('Connected to Redis Sentinel');
-}).on('error', e => {
-    console.error('error during connecting', e.message || 'no e obj');
-});
-
-// Publish a message to a channel
-sentinelClient.publish('myChannel', 'Hello, Redis Sentinel!')
-    .then((reply) => {
+async function handleConnect(client) {
+    try {
+        // Publish a message to a channel
+        const reply = await client.publish('myChannel', 'Hello, Redis Sentinel!');
         console.log('Message published:', reply);
-    })
-    .catch((err) => {
-        console.error('Error publishing message:', err);
-    });
 
-// Subscribe and listen for messages on a channel
-const subscriber = new Redis();
-subscriber.on('message', (channel, message) => {
-    console.log('Received message from', channel, ':', message);
-});
+        // Subscribe and listen for messages on a channel
+        const subscriber = new Redis();
+        subscriber.on('message', (channel, message) => {
+            console.log('Received message from', channel, ':', message);
+        });
 
-subscriber.subscribe('myChannel', (err, count) => {
-    if (err) {
-        console.error('Error subscribing to channel:', err);
-    } else {
+        const count = await subscriber.subscribe('myChannel');
         console.log('Subscribed to', count, 'channel(s)');
+    } catch (error) {
+        console.error('Error in async', error);
     }
-});
+}
+
+async function main() {
+
+    // Create Redis Sentinel client
+    const sentinelClient = new Redis(sentinelOptions);
+
+    // Connect to the Sentinel
+    sentinelClient.on('connect', async () => {
+        console.log('Connected to Redis Sentinel');
+        await handleConnect(sentinelClient)
+    }).on('error', (e) => {
+        console.error('Error:', e);
+    });
+}
+main();
